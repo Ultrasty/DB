55386285ce85244c831c5bb555f2752861746cdd
Ultrasty <1455670697@qq.com>
2020-09-10 01:45:17 +0800

    添加了前台查询

--- api/sty/Startup.cs
+++ api/sty/Startup.cs
@@ -30,6 +30,15 @@ namespace API1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(options =>
+            {
+                options.AllowAnyHeader();
+                options.AllowAnyMethod();
+                options.AllowAnyOrigin();
+                options.AllowCredentials();
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
