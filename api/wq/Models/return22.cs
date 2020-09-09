cd279f0f50762d71a3a633298a5010dca40a3ebf
王乾 <58730779+huajiaohuixiang@users.noreply.github.com>
2020-09-09 19:23:21 +0800

    Add files via upload

---
+++ api/wq/Models/return22.cs
@@ -0,0 +1,11 @@
+
+    public class return22{
+        public string stu_id{get;set;}
+        public string stu_name{get;set;}
+        public string  date{get;set;}
+        public string trip_num{get;set;}
+
+
+    }
+    
+
