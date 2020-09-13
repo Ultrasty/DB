d0546778f01a36c2c0e0460b31b4f74a098f07bf
王乾 <58730779+huajiaohuixiang@users.noreply.github.com>
2020-09-13 08:52:10 +0800

    Add files via upload

---
+++ api/wq/Models/return19.cs
@@ -0,0 +1,17 @@
+using System;
+public class return19{
+public	string	student_ID	{get;set;}
+public	string	student_name	{get;set;}
+
+public	string	batch_num	{get;set;}
+
+public	DateTime	date	{get;set;}
+
+
+}
+
+public class batch{
+
+    public string batch_num;
+    public string date;
+}
