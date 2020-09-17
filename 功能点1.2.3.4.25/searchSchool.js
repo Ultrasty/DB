28cbe14fac4b9546767d7add0ce582f3afc2e52f
Mattexplosm <43245995+Mattexplosm@users.noreply.github.com>
2020-09-17 22:53:11 +0800

    Add files via upload

---
+++ 功能点1.2.3.4.25/searchSchool.js
@@ -0,0 +1,37 @@
+var abc = new Vue({
+  el: "#app",
+  data: {
+      message: "hello",
+      id:"",
+      result:"",
+      building:"",
+      name:"",
+      phonenumber:""
+  },
+  methods: {
+      submit: function () {
+              var url = "http://121.199.77.139:8090/department/check";
+              var that=this;
+              axios.post(
+                  url, {
+                      "id":this.id
+                  }).then(function (response) {
+                  console.log(response.data.result);
+                 that.result=response.data.result;
+                 if(that.result==="wrong id")
+                 alert("id不存在");
+                 if(that.result==="success")
+                 alert("查找成功");
+                 console.log(response.data.building);
+                 that.building=response.data.building;
+                 console.log(response.data.name);
+                 that.name=response.data.name;
+                 console.log(response.data.phonenumber);
+                 that.phonenumber=response.data.phonenumber;
+              }, function (err) {
+                  console.log(err);
+              })
+          }
+      
+  }
+})
