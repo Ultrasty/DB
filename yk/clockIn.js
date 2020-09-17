bc49378b524a3f39989ae29da4dd492938fb8318
Ultrasty <1455670697@qq.com>
2020-09-18 01:03:46 +0800

    目录

---
+++ yk/clockIn.js
@@ -0,0 +1,34 @@
+var abc = new Vue({
+    el: "#app",
+    data: {
+        message: "hello",
+        id:"",
+        facilityID:"",
+        inOut:"",
+        result:""
+    },
+    methods: {
+        submit: function () {
+                var url = "http://121.199.77.139:8090/facility/sign";
+                var that=this;
+                console.log("123");
+                axios.post(
+                    url, {
+                        "id": this.studentID,
+                       "facility":this.facilityID,
+                       "inout":this.inOut
+                    }).then(function (response) {
+                    console.log(response.data);
+                    that.result=response.data;
+                    if(that.result==="打卡成功")
+                    alert("打卡成功");
+                    else
+                    alert("打卡失败");
+
+                }, function (err) {
+                    console.log(err);
+                })
+            }
+        
+    }
+})
